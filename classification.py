1d1cb4d399582f34d224ab81c908c30782b0b5fc
Bijan Fakhri <bfakhri@asu.edu>
2019-02-20 14:35:57 -0700

    Added support for arbitrary datasets

--- classification.py
+++ classification.py
@@ -19,11 +19,11 @@ act_func = tf.nn.tanh
 SESS_CONFIG = tf.ConfigProto(device_count = {'GPU': 1})
 
 # Define variable functions
-def weight_variable(shape, name="W"):
+def weight_variable(shape, name='W'):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial, name=name)
 
-def bias_variable(shape, name="B"):
+def bias_variable(shape, name='B'):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial, name=name)
 
@@ -39,32 +39,32 @@ def max_pool_2x2(x, name='max_pool'):
         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 # Define a Convolutional Layer 
-def conv_layer(x, fan_in, fan_out, name="convl"):
+def conv_layer(x, fan_in, fan_out, name='convl'):
     with tf.name_scope(name):
         # Create Weight Variables
-        W = weight_variable([5, 5, fan_in, fan_out], name="W")
-        B = bias_variable([fan_out], name="B")
+        W = weight_variable([5, 5, fan_in, fan_out], name='W')
+        B = bias_variable([fan_out], name='B')
         # Convolve the input using the weights
         conv = conv2d(x, W)
         # Push input+bias through activation function
         activ = tf.nn.relu(conv + B)
         # Create histograms for visualization
-        tf.contrib.summary.histogram("Weights", W)
-        tf.contrib.summary.histogram("Biases", B)
-        tf.contrib.summary.histogram("Activations", activ) 
+        tf.contrib.summary.histogram('Weights', W)
+        tf.contrib.summary.histogram('Biases', B)
+        tf.contrib.summary.histogram('Activations', activ) 
         # MaxPool Output
         return max_pool_2x2(activ)
 
-def conver(x, w, b, name="convll"):
+def conver(x, w, b, name='convll'):
     with tf.name_scope(name):
         # Convolve the input using the weights
         conv = conv2d(x, w)
         # Push input+bias through activation function
         activ = tf.nn.relu(conv + b)
         # Create histograms for visualization
-        tf.contrib.summary.histogram("Weights", w)
-        tf.contrib.summary.histogram("Biases", b)
-        tf.contrib.summary.histogram("Activations", activ) 
+        tf.contrib.summary.histogram('Weights', w)
+        tf.contrib.summary.histogram('Biases', b)
+        tf.contrib.summary.histogram('Activations', activ) 
         # MaxPool Output
         return max_pool_2x2(activ)
 
@@ -72,43 +72,41 @@ summary_writer = tf.contrib.summary.create_file_writer(LOGDIR, flush_millis=100)
 summary_writer.set_as_default()
 global_step = tf.train.get_or_create_global_step()
 
-# Get Data
-# Construct a tf.data.Dataset
-(ds_train, ds_test), ds_info = tfds.load(name="mnist", split=["train", "test"], with_info=True)
-img_shape = tf.TensorShape(ds_info.features['image'].shape)
-SIZE_X = 28
-SIZE_Y = 28
-NUM_CLASSES = 10 
 
 class Model:
     ' Simple Image Classification Model (defined by CNN) ' 
 
-    def __init__(self, learn_rate=1e-4):
+    def __init__(self, input_shape, output_size, learn_rate=1e-4):
         ' Initializes model parameters and optimizer ' 
 
         # Stores model params
         self.vars = []
         ## Conv Layers
-        ds_chans = img_shape[2]
-        fn_out = 32*ds_chans
-        self.conv1_w = weight_variable([5, 5, ds_chans, fn_out], name="W")
-        self.conv1_b = bias_variable([fn_out], name="B")
+        #self.ds_chans = input_shape[2]
+        self.input_shape = input_shape
+        self.output_size = output_size
+        next_size = int((input_shape[0]+1)/2)
+        fn_out = 32*self.input_shape[2]
+        self.conv1_w = weight_variable([5, 5, self.input_shape[2], fn_out], name='W')
+        self.conv1_b = bias_variable([fn_out], name='B')
         self.vars.append(self.conv1_w)
         self.vars.append(self.conv1_b)
         fn_in = fn_out
         fn_out = 2*fn_in 
-        self.conv2_w = weight_variable([5, 5, fn_in, fn_out], name="W")
-        self.conv2_b = bias_variable([fn_out], name="B")
+        next_size = int((next_size+1)/2)
+        self.conv2_w = weight_variable([5, 5, fn_in, fn_out], name='W')
+        self.conv2_b = bias_variable([fn_out], name='B')
         self.vars.append(self.conv2_w)
         self.vars.append(self.conv2_b)
         ## FC Layers
         self.FC1_Size = 512 
-        self.W_fc_up1 = weight_variable([7*7*fn_out, self.FC1_Size])
+        
+        self.W_fc_up1 = weight_variable([next_size*next_size*fn_out, self.FC1_Size])
         self.b_fc_up1 = bias_variable([self.FC1_Size])
         self.vars.append(self.W_fc_up1)
         self.vars.append(self.b_fc_up1)
-        self.W_fc_up2 = weight_variable([self.FC1_Size, NUM_CLASSES])
-        self.b_fc_up2 = bias_variable([NUM_CLASSES])
+        self.W_fc_up2 = weight_variable([self.FC1_Size, output_size])
+        self.b_fc_up2 = bias_variable([output_size])
         self.vars.append(self.W_fc_up2)
         self.vars.append(self.b_fc_up2)
 
@@ -122,7 +120,7 @@ class Model:
         with tf.name_scope('MainGraph'):
             with tf.name_scope('Inputs'):
                 # Reshape X to make it into a 2D image
-                x_image = tf.reshape(x_input, [-1, SIZE_X, SIZE_Y, 1])
+                x_image = tf.reshape(x_input, [-1,]+self.input_shape)
                 tf.contrib.summary.image('original_image', x_image, max_images=3)
             # FC Encoder Layers
             with tf.name_scope('Convs'):
@@ -148,7 +146,6 @@ class Model:
         with tf.GradientTape() as tape:
             output = self.crunch(x_input)
             with tf.name_scope('Generation_Loss'):
-                x_rs = tf.reshape(x_input, (-1, SIZE_X*SIZE_Y))
                 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=output))
                 tf.contrib.summary.scalar('Cross Entropy', cross_entropy)
 
@@ -175,15 +172,27 @@ class Model:
         val_acc = self.calc_accuracy(output, labels, is_val=True)
         return val_acc
 
+# Get Data
+# Construct a tf.data.Dataset
+#ds_name = 'mnist'
+#ds_name = 'cifar10'
+ds_name = 'cifar100'
+#ds_name = 'omniglot'
+(ds_train, ds_test), ds_info = tfds.load(name=ds_name, split=['train', 'test'], with_info=True)
+img_shape = tf.TensorShape(ds_info.features['image'].shape)
+label_shape = tf.TensorShape(ds_info.features['label'].shape)
+num_classes = ds_info.features['label'].num_classes
+print('DS Shape: ')
+print(img_shape, num_classes)
 
 
 # Creates a classifier model
-model = Model()
+model = Model(img_shape.as_list(), num_classes)
 
 # Preparing datasets (training and validation)
 # Batch size of 1024 the repeats when iterated through
-ds_train = ds_train.batch(1024).repeat()
-ds_test = ds_test.batch(1024).repeat()
+ds_train = ds_train.batch(256).repeat()
+ds_test = ds_test.batch(256).repeat()
 
 # Converts validation set into an iterator so we can iterate through it
 ds_test_iter = iter(ds_test)
@@ -192,11 +201,11 @@ ds_test_iter = iter(ds_test)
 for idx,batch in enumerate(ds_train):
     # Prepare training inputs
     x_inputs = tf.math.divide(tf.cast(batch['image'], tf.float32), tf.constant(255.0, dtype=tf.float32))
-    y_labels = tf.one_hot(batch['label'], depth=NUM_CLASSES)
+    y_labels = tf.one_hot(batch['label'], depth=num_classes)
     # Prepare validation inputs
     val_batch = next(ds_test_iter)
     val_x_inputs = tf.math.divide(tf.cast(val_batch['image'], tf.float32), tf.constant(255.0, dtype=tf.float32))
-    val_y_labels = tf.one_hot(val_batch['label'], depth=NUM_CLASSES)
+    val_y_labels = tf.one_hot(val_batch['label'], depth=num_classes)
     # Train and validate
     with tf.contrib.summary.record_summaries_every_n_global_steps(100):
         preds, loss = model.learn(x_inputs, y_labels) 
